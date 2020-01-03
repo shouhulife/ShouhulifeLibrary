bbdc6b6e66144286184082fbed7b1c2f21288f72
shouhulife <shouhulife@gmail.com>
2020-01-03 14:37:14 +0800

    日期间隔工具

--- utilslibrary/src/main/java/com/shouhulife/utilslibrary/util/DateDistance.java
+++ utilslibrary/src/main/java/com/shouhulife/utilslibrary/util/DateDistance.java
@@ -2,10 +2,67 @@ package com.shouhulife.utilslibrary.util;
 
 import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateDistance {
+
+    private final static long minute = 60 * 1000;// 1分钟
+    private final static long hour = 60 * minute;// 1小时
+    private final static long day = 24 * hour;// 1天
+    private final static long month = 31 * day;// 月
+    private final static long year = 12 * month;// 年
+
+    /**
+     * 返回文字描述的日期
+     *
+     * @param date
+     * @param
+     * @return
+     */
+    public static String getTimeFormatText(Date date) {
+        if (date == null) {
+            return null;
+        }
+        long diff = new Date().getTime() - date.getTime();
+        long r = 0;
+        if (diff > year) {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            String dateString = formatter.format(date);
+            return dateString ;//+ "带年"
+        }
+
+        if (diff > day && diff <year) {
+            r = (diff / day);
+            SimpleDateFormat formatter = new SimpleDateFormat("MM-dd");
+            String dateString = formatter.format(date);
+            return dateString ;//+ "不带年"
+        }
+        if (diff > hour && diff < day) {
+            r = (diff / hour);
+            return r + "个小时前";
+        }
+        if (diff > minute && diff < hour) {
+            r = (diff / minute);
+            return r + "分钟前";
+        }
+        return "刚刚";
+    }
+
+    /**
+     * 将长时间格式字符串转换为Date时间 yyyy-MM-dd HH:mm:ss
+     *
+     * @param strDate
+     * @return
+     */
+    public static Date strToDateLong(String strDate) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        ParsePosition pos = new ParsePosition(0);
+        Date strtodate = formatter.parse(strDate, pos);
+        return strtodate;
+    }
+
     /**
      * 两个时间之间相差距离多少分钟
      * @param str1 时间参数 1：
